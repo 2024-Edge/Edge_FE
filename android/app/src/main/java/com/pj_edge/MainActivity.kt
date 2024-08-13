4198ff1ce5b6ee4b97933a44301a9184ced532fe
jiwonee02 <nepthun@naver.com>
2024-08-13 20:10:26 +0900

    안드로이드 스플래쉬

--- android/app/src/main/java/com/pj_edge/MainActivity.kt
+++ android/app/src/main/java/com/pj_edge/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.pj_edge
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)  // 여기
+    super.onCreate(savedInstanceState)
+}
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "pj_edge"
+  override fun getMainComponentName(): String {
+    return "pj_edge"
+}
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
